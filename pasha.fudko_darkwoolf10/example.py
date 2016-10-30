29a409567e66c7f015c9cb31916a31b537d7efed
darkwoolf10 <darkwoolf10@woolftreuser@yandex.ru>
2016-10-30 17:22:59 +0200

    homeworks_one

---
+++ pasha.fudko_darkwoolf10/example.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+city = input("Where do you live? ")
+
+print("Ah, so your name id %s, your age is %i, "
+       "and you live in %s. Fantastic!" % (name, age, city))
