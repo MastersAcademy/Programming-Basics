297aa2772d66e0b48a7f5ff79220504a205bf998
rrLero <roman@xlan.net.ua>
2016-11-07 23:25:41 +0200

    Homework - 1 Added

--- homeworks/roman.levchenko_rrLero/homework-1/myFirstProgram.py
+++ homeworks/roman.levchenko_rrLero/homework-1/myFirstProgram.py
@@ -3,39 +3,37 @@
 # Roman Levchenko
 
 f = open('output.txt')
-f2 = open('data.txt','w')
+f2 = open('data.txt', 'w')
 
 print("What is your name? ")
 name = f.readline()
 print(name)
-print()
+
 
 print("Why do you study PYTHON?")
 answer_1 = f.readline()
 print(answer_1)
-print()
+
 
 print("How do you rate MasterAcademy (1-10)?")
-answer_2 = f.readline()
+answer_2 = int(f.readline())
 print(answer_2)
-print()
+
 
 print("What is your hobby?")
 answer_3 = f.readline()
 print(answer_3)
-print()
 
-print("So mr.",name,"You study Python because",answer_1)
 
+print("So mr. %s You study Python because %s" % (name, answer_1))
 print("and you like", answer_3)
-
 print("Your rating of Mater Academy is", answer_2, "Describe why?")
 
-#Writing answer in data file
+# Writing answer in data file
 data = f2.write(input())
 f.close()
 f2.close()
 
 # JOKE
-#- How did you spent holidays?
-#- was lying on the couch, then i tired and went sleep
+# - How did you spent holidays?
+# - was lying on the couch, then i tired and went sleep
