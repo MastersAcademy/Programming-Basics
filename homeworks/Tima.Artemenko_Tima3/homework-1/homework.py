4019dee0702cf8e7cd7ba02d099d4bb093561e4a
Tima3 <artemenkotima9@gmail.com>
2016-11-08 19:32:09 +0200

    Homework added

--- homeworks/Tima.Artemenko_Tima3/homework-1/homework.py
+++ homeworks/Tima.Artemenko_Tima3/homework-1/homework.py
@@ -1,17 +1,10 @@
-import sys
+name = input("Enter your name:")
+surname = input("Enter your surname:")
+age = int(input("Enter your age:"))
+car = input("What is your car?:")
+hometown = input("Where are you from?:")
 
-my_file = open ('file.txt', 'w')
-
-print("process started! ")
-
-name = print(input("what is your name dude? :"))
-age = print(input("how old are you? :"))
-email = print(input("pelase enter your email :"))
-
-my_file.write ("Hello , now you see my name , age , and ( email ) \r\n")
-my_file.write ("bye all , nice to meet you !")
-
-print("process finished, bye! ")
-
-my_file.close()
-
+output_text = "Your name is %s %s, You are %i years old, Your car is %s, You are from %s!" %(name,surname,age,car,hometown)
+output_file = open("output.txt", "w")
+output_file.write(output_text)
+output_file.close()
