5d750cb229260d069c21d824352286ac90e950f6
Valik <valikpost8@gmail.com>
2016-11-11 16:25:26 +0200

    homeworks.py
    1

---
+++ homeworks/Valik.Pavlenko_ValikPavlenko/homeworks-1/homeworks.py
@@ -0,0 +1,5 @@
+name =input("What you name? ")
+age =int(input("How old are you? "))
+city =input("Where do you live? ")
+
+print("Ah, so your name is %s, your age is %s, and you live in %s. " % (name, age, city))
