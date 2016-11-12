9b264a5ca00773e29fdd8b06a7f6a804a553a627
Michael Savchenko <michaelsav4enko@gmail.com>
2016-11-12 13:49:29 +0200

    create file info.txt

--- homeworks/michael.savchenko_MichaelSav4enko/Homework-1/questionnaire.py
+++ homeworks/michael.savchenko_MichaelSav4enko/Homework-1/questionnaire.py
@@ -4,10 +4,10 @@ surname = input ("What is your surname? ")
 age = int(input("How old are you? "))
 city = input ("Where do you live? ")
 
-print ("So your name is %s %s, and you are %i years old. "
+result = ("So your name is %s %s, and you are %i years old. "
        " %s is qreat city!" % (name, surname, age, city))
+print (result)
 
-result = "name is %s, surname is %s, age is s%, city is %s" % (name, surname, age, city)
 file = open ("info.txt", "w")
 file.write(result)
 file.close()
