ffbc2077d3ad90ab21c53159b9088a9643b08c93
Dmitry Zhuk <zhuchara@gmail.com>
2016-11-13 13:46:09 +0200

    additional data added to our dictionary

--- sessions/02 – Collections/Dictionaries.py
+++ sessions/02 – Collections/Dictionaries.py
@@ -7,3 +7,26 @@ dict_a = {
 }
 print dict_a
 print dict_a['first_name']
+
+for k, v in dict_a.items():
+    print k + ': ' + v
+
+dict_a['hobbies'] = ['computer games', 'reading', 'studying']
+print dict_a
+
+dict_b = {
+    'cat': {
+        'name': 'Hort',
+        'color': 'black',
+        'age': '3'
+    },
+    'dog': {
+        'name': 'Bars',
+        'color': 'white',
+        'age': '11'
+    }
+}
+
+dict_a['pet'] = dict_b
+print dict_a
+
