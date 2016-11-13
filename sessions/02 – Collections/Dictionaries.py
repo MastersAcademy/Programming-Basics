79b29752e9cc0733421264b6ca117ce197986331
Dmitry Zhuk <zhuchara@gmail.com>
2016-11-13 13:41:23 +0200

    dictionary example added, printing to console added

---
+++ sessions/02 – Collections/Dictionaries.py
@@ -0,0 +1,9 @@
+print '--- === *** Dictionaries *** === --- '
+
+dict_a = {
+    'first_name': 'Dmitry',
+    'last_name': 'Zhuk',
+    'dob': '1987-04-11'
+}
+print dict_a
+print dict_a['first_name']
