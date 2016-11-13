1cd8125885f466186bfc2dfc476649f624df7f26
Innat1 <innat.ck@gmail.com>
2016-11-13 21:43:08 +0200

    Adding homework1

--- homeworks/Inna.Taranenko_Innat1/homework1/homework1.py
+++ homeworks/Inna.Taranenko_Innat1/homework1/homework1.py
@@ -1,21 +1,14 @@
-name = str(input("What is your name?"))
+name = input("What is your name?")
 print ("Hello, " + name + "!!!")
-start = input("Would you like to answer some questions?")
 
-if start == 'yes':
-    print ("Ok, let's go!")
-    surname = input("What is your surname?")
-    birth = input("What is your date of birth?")
-    adress = input("Where are you from?")
-    print ("Thank's for information! Good buy!")
-    
-else:
-    print ("It's a pity, good buy!")
+surname = input("What is your surname?")
+birth = input("What is your date of birth?")
+adress = input("Where are you from?")
+print ("Thank's for information! Good buy!")
 
+textdoc = name +" " + surname + " was born in " + adress + " in " + birth
+print (textdoc)
 
 f = open('D:\LearningPython\saveName.txt','w')
-f.write (name)
-f.write (surname)
-f.write (birth)
-f.write (adress)
+f.write (textdoc)
 f.close ()
