93837046c32044d9e92dc24390d8a1dc9e60ac6d
Pavlo Havronenko <344505@gmail.com>
2016-11-15 15:11:11 +0200

    Homework 2 added, now with dictionary

---
+++ homeworks/pavlo.havronenko_hex-344505/homework-2/homework-2.py
@@ -0,0 +1,28 @@
+import os
+import datetime
+data_path = './DATA.csv'
+user = dict()
+print ("Please provide some information for statistic")
+user["Name"] = input ("What is Your Name => ")
+user["Location"] = input ("Where are You From => ")
+cust_age = input ("How Old Are You => ")
+
+
+if not cust_age.isdigit():
+    cust_age = input ("Provide only numders for age, try once again => ")
+user["Birth"] = str(datetime.datetime.now().year - int(cust_age))
+
+print ("So your name %s, you from %s, and you was born in %s " %(user["Name"], user["Location"], user["Birth"]))
+
+if os.path.exists(data_path):
+    data_file = open (data_path,'a')
+    data_file.write(user["Name"] + ',' + user["Location"] + ',' + user["Birth"] + '\n')
+    data_file.close()
+else:
+    data_file = open (data_path, 'w')
+    data_file.write("Name,Location,Year of Birth\n")
+    data_file.write(user["Name"] + ',' + user["Location"] + ',' + user["Birth"] + '\n')
+    data_file.close()
+
+print ("Data SAVED to %s" %(data_path))
+
