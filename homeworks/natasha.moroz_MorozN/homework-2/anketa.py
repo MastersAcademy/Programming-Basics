38e5664e32f358fdae0e85b38f112ea50eb9a259
MorozN <moroz.natasha@mail.ru>
2016-11-16 21:18:47 +0300

    create anketa

---
+++ homeworks/natasha.moroz_MorozN/homework-2/anketa.py
@@ -0,0 +1,10 @@
+
+print "Let me introduce myself"
+
+dict_anketa = {'name':'Natalia', 'surname':'Moroz', 'hobies':'read a book'}
+print (dict_anketa)
+
+file = open("howework2.txt",  "w")
+file.write(str(file))
+file.close()
+
