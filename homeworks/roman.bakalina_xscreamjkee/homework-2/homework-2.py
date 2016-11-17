44488eba25e3a3e3e40de3eed700bdc5cec0d67c
xScreamjkee <xScreamjkee@gmail.com>
2016-11-17 23:12:52 +0200

    upgrade homework2

--- homeworks/roman.bakalina_xscreamjkee/homework-2/homework-2.py
+++ homeworks/roman.bakalina_xscreamjkee/homework-2/homework-2.py
@@ -1,42 +1,27 @@
 #!/usr/bin/env python3
-
 print("\n     	 homework-2")
-print("\n>>Create file and enter some information in the dictionary<<\n")
+print("\n>>Create file and enter some information <<\n")
 
 #entering information
 f_name = input("Enter the file name: ")
 f_name = f_name+".txt"
-print("\n>>Your file is created: ",f_name,"\n")
-word = input("\nEnter a word or phrase that you want add to the your file: ")
-name = input("\nEnter your nickname or name: ")
-age  = input("\nEnter your age: ")
-country = input("\nEnter your country: ")
+print("\n>>Your file is created: ",f_name)
 
-#dictionary
-diction = {
-		"word or phrase": word,
-		"name":name,
-		"age":age,
-		"country":country
-		}
+coll = {}
+coll['word'] = input("\nEnter a word or phrase that you want add to the your file: ")
+coll['name'] = input("\nEnter your nickname or name: ")
+coll['age']  = input("\nEnter your age: ")
+coll['country'] = input("\nEnter your country: ")
+
+info = "\nSome of the information you entered:\nWord: %s\nNickname: %s\nAge: %s\nCountry: %s"%(coll['word'], coll['name'], coll['age'], coll['country'])
+print(info)
 
 #write text on file
-f_txt = open(f_name, 'w')
-f_txt.write(str(diction))
+f_txt = open(f_name,'w')
+f_txt.write(info)
 f_txt.close()
-
-
-#read text from file
-print("\n>>Information that you added to file:\n")
-f_txt = open(f_name, 'r')
-line = f_txt.readline()
-while line:
-	print (line)
-	line = f_txt.readline()
-f_txt.close()
-print("\n>>Your file is saved.<<\n")
-
+print(">>Your file is saved.<<\n")
 
 #exit
 input("Press 'Enter' to exit:")
-print("\nGoodbay,",name)
+print("\nGoodbay,",coll['name'])
