c217229c41e2de7d55c57d52cb2a8f7aed97a42a
Alexey Tetievskiy <abrek-999@mail.ru>
2016-11-18 02:39:41 +0300

    Added dictionary

--- homeworks/alexey.tetievskiy_AlexeyTetievskiy/homework1/homework.py
+++ homeworks/alexey.tetievskiy_AlexeyTetievskiy/homework1/homework.py
@@ -5,6 +5,14 @@ city = input("Где вы живете?")
 print("Ага, значит вас зовут %s, вам %i лет,"
       "и вы живете в %s. Ого!" % (name,age,city))
 
+dict_a = {
+      'first_name': name,
+      'age':age,
+      'city':city
+}
+
+print(dict_a)
+
 outputfile ="Data.txt"
 
 datafile = open(outputfile, mode='w')
