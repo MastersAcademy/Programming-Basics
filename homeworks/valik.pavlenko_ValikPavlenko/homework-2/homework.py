7a7ae3548aa810706e47701a1549ecebfe25778c
Valik <valikpost8@gmail.com>
2016-11-18 19:06:04 +0200

    Create homework.py

---
+++ homeworks/valik.pavlenko_ValikPavlenko/homework-2/homework.py
@@ -0,0 +1,10 @@
+personal_information ={
+    "name":"valik",
+    "last name":"Pavlenko",
+    "dob":"14-02-1999",
+    "lives": "Cherkasy"
+}
+
+personal_information ["Hobbias"]="Reading", 
+for k, v in personal_information.items():
+    print ('{0}: {1}'.format(k, v))
