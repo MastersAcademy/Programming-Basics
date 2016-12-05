709468e498bbd6475a6107876a2e672f7b2fcd75
Pavlo Havronenko <344505@gmail.com>
2016-12-05 23:10:13 +0200

    Homework 4 , range fixes

--- homeworks/pavlo.havronenko_hex-344505/homework-4/homework-4.py
+++ homeworks/pavlo.havronenko_hex-344505/homework-4/homework-4.py
@@ -1,8 +1,9 @@
 import os, datetime
 #import urllib3
 
-days = range(31)
-month = range(12)
+days = range(1,32,1)
+month = range(1,13,1)
+print(month)
 
 cust_birth = dict()
 #http = urllib3.PoolManager()
@@ -16,28 +17,28 @@ while mn == 0 :
     else :
         print ("Take another try...")
 
-if ((cust_birth["month"] == 1) and (cust_birth["day"] in range(21,31,1))) or ((cust_birth["month"] == 2) and (cust_birth["day"] in range(19))):
+if ((cust_birth["month"] == 1) and (cust_birth["day"] in range(21,32,1))) or ((cust_birth["month"] == 2) and (cust_birth["day"] in range(20))):
     print ("Your zodiac sign Aquarius")
-elif ((cust_birth["month"] == 2) and (cust_birth["day"] in range(20,31,1))) or ((cust_birth["month"] == 3) and (cust_birth["day"] in range(20))):
+elif ((cust_birth["month"] == 2) and (cust_birth["day"] in range(20,29,1))) or ((cust_birth["month"] == 3) and (cust_birth["day"] in range(21))):
     print ("Your zodiac sign Pisces")
-elif ((cust_birth["month"] == 3) and (cust_birth["day"] in range(21,31,1))) or ((cust_birth["month"] == 4) and (cust_birth["day"] in range(20))):
+elif ((cust_birth["month"] == 3) and (cust_birth["day"] in range(21,32,1))) or ((cust_birth["month"] == 4) and (cust_birth["day"] in range(21))):
     print ("Your zodiac sign Aries")
-elif ((cust_birth["month"] == 4) and (cust_birth["day"] in range(21,31,1))) or ((cust_birth["month"] == 5) and (cust_birth["day"] in range(20))):
+elif ((cust_birth["month"] == 4) and (cust_birth["day"] in range(21,31,1))) or ((cust_birth["month"] == 5) and (cust_birth["day"] in range(21))):
     print ("Your zodiac sign Taurus")
-elif ((cust_birth["month"] == 5) and (cust_birth["day"] in range(21,31,1))) or ((cust_birth["month"] == 6) and (cust_birth["day"] in range(21))):
+elif ((cust_birth["month"] == 5) and (cust_birth["day"] in range(21,32,1))) or ((cust_birth["month"] == 6) and (cust_birth["day"] in range(22))):
     print ("Your zodiac sign Gemini")
-elif ((cust_birth["month"] == 6) and (cust_birth["day"] in range(22,31,1))) or ((cust_birth["month"] == 7) and (cust_birth["day"] in range(22))):
+elif ((cust_birth["month"] == 6) and (cust_birth["day"] in range(22,31,1))) or ((cust_birth["month"] == 7) and (cust_birth["day"] in range(23))):
     print ("Your zodiac sign Cancer")
-elif ((cust_birth["month"] == 7) and (cust_birth["day"] in range(23,31,1))) or ((cust_birth["month"] == 8) and (cust_birth["day"] in range(23))):
+elif ((cust_birth["month"] == 7) and (cust_birth["day"] in range(23,32,1))) or ((cust_birth["month"] == 8) and (cust_birth["day"] in range(24))):
     print ("Your zodiac sign Lion")
-elif ((cust_birth["month"] == 9) and (cust_birth["day"] in range(24,31,1))) or ((cust_birth["month"] == 9) and (cust_birth["day"] in range(23))):
+elif ((cust_birth["month"] == 8) and (cust_birth["day"] in range(24,32,1))) or ((cust_birth["month"] == 9) and (cust_birth["day"] in range(24))):
     print ("Your zodiac sign Virgo")
-elif ((cust_birth["month"] == 9) and (cust_birth["day"] in range(24,31,1))) or ((cust_birth["month"] == 10) and (cust_birth["day"] in range(23))):
+elif ((cust_birth["month"] == 9) and (cust_birth["day"] in range(24,31,1))) or ((cust_birth["month"] == 10) and (cust_birth["day"] in range(24))):
     print ("Your zodiac sign Libra")
-elif ((cust_birth["month"] == 10) and (cust_birth["day"] in range(24,31,1))) or ((cust_birth["month"] == 11) and (cust_birth["day"] in range(22))):
+elif ((cust_birth["month"] == 10) and (cust_birth["day"] in range(24,32,1))) or ((cust_birth["month"] == 11) and (cust_birth["day"] in range(23))):
     print ("Your zodiac sign Scorpio")
-elif ((cust_birth["month"] == 11) and (cust_birth["day"] in range(23,31,1))) or ((cust_birth["month"] == 12) and (cust_birth["day"] in range(21))):
+elif ((cust_birth["month"] == 11) and (cust_birth["day"] in range(23,31,1))) or ((cust_birth["month"] == 12) and (cust_birth["day"] in range(22))):
     print ("Your zodiac sign Sagittarius")
-else:
+elif ((cust_birth["month"] == 12) and (cust_birth["day"] in range(22,32,1))) or ((cust_birth["month"] == 1) and (cust_birth["day"] in range(21))):
     print ("Your zodiac sign Capricorn")
 
