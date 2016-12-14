da7ca9c65bb311706f34fb22c6a4b7a07ca6b399
Dmitry Zhuk <dmitry.zhuk.dev@gmail.com>
2016-12-15 00:45:51 +0200

    session 05 added

---
+++ sessions/05 - Classes/User.py
@@ -0,0 +1,12 @@
+class User(object):
+    def __init__(self, email, first_name, last_name, user_role):
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_role = user_role
+
+    def __str__(self):
+        return "email: %s, first_name: %s, last_name: %s, user_role: %s" % (self.email, self.last_name, self.last_name, self.user_role)
+
+    def full_name(self):
+        return "%s %s" % (self.first_name, self.last_name)
