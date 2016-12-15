3454755515653bb10af60bfa5c22ce7b53e7b956
rrlero <roman@xlan.net.ua>
2016-12-15 14:22:32 +0200

    little remark

--- sessions/05 - Classes/User.py
+++ sessions/05 - Classes/User.py
@@ -6,7 +6,7 @@ class User(object):
         self.user_role = user_role
 
     def __str__(self):
-        return "email: %s, first_name: %s, last_name: %s, user_role: %s" % (self.email, self.last_name, self.last_name, self.user_role)
+        return "email: %s, first_name: %s, last_name: %s, user_role: %s" % (self.email, self.first_name, self.last_name, self.user_role)
 
     def full_name(self):
         return "%s %s" % (self.first_name, self.last_name)
