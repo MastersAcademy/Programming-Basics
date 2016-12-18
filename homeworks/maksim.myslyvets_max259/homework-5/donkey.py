e77e77fc5f1245c595bde75c6570237f6871672e
max259 <max259@gmail.com>
2016-12-18 16:39:43 +0200

    add object2

---
+++ homeworks/maksim.myslyvets_max259/homework-5/donkey.py
@@ -0,0 +1,12 @@
+from mammals import mammals
+
+
+
+class donkey(mammals):
+    def __init__(self, viviparous, warm_blooded, wool, mammals, increased_endurance, ruggledness, long_ears):
+        self.increased_endurance = increased_endurance
+        self.ruggledness = ruggledness
+        self.long_ears = long_ears
+
+    def __str__(self):
+        return "viviparous: %s, warm_blooded: %s, wool: %s, mammals: %s, increased_endurance: %s, ruggledness: %s, long_ears: %s" % (self.viviparous, self.warm_blooded, self.wool, self.mammals, self.increased_endurance, self.ruggledness, self.long_ears)
