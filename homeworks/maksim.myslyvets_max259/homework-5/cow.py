71bb6bf46a64e91b55e90cd269edfe010fff5acc
max259 <max259@gmail.com>
2016-12-18 16:40:06 +0200

    add object3

---
+++ homeworks/maksim.myslyvets_max259/homework-5/cow.py
@@ -0,0 +1,12 @@
+from mammals import mammals
+
+
+
+class horse(mammals):
+    def __init__(self, viviparous, warm_blooded, wool, mammals, giving_meat, geaving_milk,):
+        self.increased_endurance = giving_meat
+        self.ruggledness = geaving_milk
+
+
+    def __str__(self):
+        return "viviparous: %s, warm_blooded: %s, wool: %s, mammals: %s, giving_meat: %s, geaving_milk: %s" % (self.viviparous, self.warm_blooded, self.wool, self.mammals, self.giving_meat, self.geaving_milk)
