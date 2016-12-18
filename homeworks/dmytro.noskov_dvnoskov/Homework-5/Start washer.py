f25dec508e3ba6acac59a1b54e35182d2f41a13d
Dmytri <dvnoskov@mail.ru>
2016-12-18 20:21:36 +0200

    Home task 5(on the task at homework-3)_edit

--- homeworks/dmytro.noskov_dvnoskov/Homework-5/Start washer.py
+++ homeworks/dmytro.noskov_dvnoskov/Homework-5/Start washer.py
@@ -15,13 +15,13 @@ e = input('specify the weight of the laundry')
 w='150vat'
 m ='manual'
 if A=='2' :
-    from washer_auto import Washer_auto
+    from Washer_auto import Washer_auto
     print('start')
     wash = Washer_auto(q,w,e,r,t)  #wash = washer_auto('hot', '150vat', '5', 'auto', '1000')
     wash.power_work()
     wash.run_1()
 else:
-    from washer_manual import Washer_manual
+    from Washer_manual import Washer_manual
     print('start')
     wash = Washer_manual(q,w,e,m) #wash = washer_manual('hot', '150vat', '5', 'manual')
     wash.power_work()
