a5ce2e9504276414fd15d863de149ad9450b6304
elmarfdk <mrfirstlove@mail.ru>
2016-12-18 22:35:00 +0200

    Homework-5

---
+++ homeworks/serhey.urakov_elmarfdk/Homework-5/Terminator.py
@@ -0,0 +1,6 @@
+class Terminator():
+    def __init__(self, model, power_source, firearms, skin):
+        self.model = model
+        self.power_source = power_source
+        self.firearms = firearms
+        self.skin = skin
