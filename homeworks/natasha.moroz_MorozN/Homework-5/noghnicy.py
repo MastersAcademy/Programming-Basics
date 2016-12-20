d89156ab2390d5b5bd172a201a2407d7100b7b76
MorozN <moroz.natasha@mail.ru>
2016-12-21 00:22:44 +0200

    homework-5

---
+++ homeworks/natasha.moroz_MorozN/Homework-5/noghnicy.py
@@ -0,0 +1,21 @@
+Class scissors
+def __init__(self,model,name, pen,blades)
+super().__init__ (model,name)
+self.pen = pen
+self.blades = blades
+ def __ thinning blade (self)
+return self.conventional blade
+
+
+
+from scissors import scissors
+def __init__(self,model,name, a pen,blades)
+scissors.__init__ (self,model,name, a pen,blades)
+
+Class thinning scissors
+def __init__(self,model,name, pen,blades)
+super().__init__ (model,name)
+self.pen = pen
+self.blades = blades
+ def __ shearer (self)
+return self.conventional blade
