61faf6ed38efb93777c4e37ea2e8db12444f9f1a
alextriam <alextriam@gmail.com>
2016-12-20 22:12:52 +0200

    homework-5
    classes

--- homeworks/oleksandr.kurov_alextriam/homework-5/classes.py
+++ homeworks/oleksandr.kurov_alextriam/homework-5/classes.py
@@ -4,18 +4,16 @@ aircraft = input("We fly on a plane or a helicopter? Type plane or helicopter ")
 trapdoor = input("Trapdoor open or close? ")
 fuel= input ("Type fuel level - full or low ")
 
-    if aircraft == 'plane':
-        weight= int (input ("Type weight your plane "))
-        from airplane import airplane
-        plane = airplane (trapdoor,fuel,weight)
-        print (plane.takeoff())
-    elif aircraft=='helicopter':
-        windspeed = int(input("Type windspeed "))
-        from helicopter import helicopter
-        helicopter = helicopter(trapdoor, fuel, windspeed)
-        print(helicopter.takeoff())
-    else:
-        print ('Is no aircraft!')
-
-
+if aircraft == 'plane':
+    weight= int (input ("Type weight your plane "))
+    from airplane import airplane
+    boing = airplane (trapdoor,fuel,weight)
+    print (boing.takeoff())
+elif aircraft=='helicopter':
+    windspeed = int(input("Type windspeed "))
+    from helicopter import helicopter
+    mi8 = helicopter(trapdoor, fuel, windspeed)
+    print(mi8.takeoff())
+else:
+    print ('Is no aircraft!')
 
