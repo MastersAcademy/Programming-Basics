c36a2845ab2a41590fe0bf6daf0fb9963522d2b2
SemyzSerhiy <80937588895@mail.ru>
2016-12-22 19:10:48 +0200

    homew5

---
+++ homeworks/serhiy.semyz_SemyzSerhiy/homework 5/Manufacturing.py
@@ -0,0 +1,6 @@
+from Ball import Ball
+
+class Manufacturing(Ball):
+    def __init__(self,name,form,material):
+        ball.__init__(self,name,form,material)
+        self.size=size
---
+++ homeworks/serhiy.semyz_SemyzSerhiy/homework 5/app.py
@@ -0,0 +1,11 @@
+from Ball import Ball
+from Football import Football
+from Manufacturing import Manufacturing
+
+
+adidas = Football('ball','football','socar ball')
+
+Ball_manufacture = Manufacturing('Ball_manufacture','Manufacturing')
+
+print('Ball')
+print('Manufacturing')
