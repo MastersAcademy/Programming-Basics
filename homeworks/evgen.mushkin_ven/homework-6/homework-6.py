7b6379c49976178f1f64647ad69081c73e4815f5
Ven <ven898989@gmail.com>
2017-01-17 21:01:21 +0200

    homework-6 add func

--- homeworks/evgen.mushkin_ven/homework-6/homework-6.py
+++ homeworks/evgen.mushkin_ven/homework-6/homework-6.py
@@ -2,13 +2,16 @@ import json
 import dicttoxml
 from SpoonBait import SpoonBait
 
+
+def create_files(obj):
+    with open('object.json', 'w') as o:
+        json.dump(obj.__dict__, o, indent=4)
+
+    with open('object.xml', 'wb+') as o:
+        o.write(dicttoxml.dicttoxml(obj.__dict__))
+
+
 spoonBait = SpoonBait('white', '70%', '12g')
-print(spoonBait.color)
-print(spoonBait.catch)
-print(spoonBait.proportions)
+print(spoonBait.__dict__)
 
-with open('object.json', 'w') as o:
-    json.dump(spoonBait.__dict__, o, indent=4)
-
-with open('object.xml', 'wb+') as o:
-    o.write(dicttoxml.dicttoxml(spoonBait.__dict__))
+create_files(spoonBait)
